b22e7d7c95fd17906c31a32705afd7cc3a169e1e
Dani Stem <57697224+Dani-Stem@users.noreply.github.com>
2026-08-28 21:55:17 -0700

    Update card_test.py

--- card_test.py
+++ card_test.py
@@ -16,9 +16,13 @@ class Deck:
     def build(self):
         suit = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
         value = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        for i in value:
+            # modifier = i
+            # new_list = []
+            new_list = [v + " of " + s for s in suit for v in value]
 
-        self.cards = [Card(suit, value) for s in suit for v in value]
 
+        print(new_list)
 
     def shuffle(self):
         if len(self.cards) > 1:
@@ -31,13 +35,4 @@ class Deck:
         return None
 
 my_deck = Deck()
-print(f"Fresh deck size: {len(my_deck.cards)} cards")
-print(f"Top 3 cards before shuffle: {my_deck.cards[:3]}\n")
-
-my_deck.shuffle()
-print("--- Deck Shuffled ---")
-print(f"Top 3 cards after shuffle: {my_deck.cards[:3]}\n")
-
-drawn_card = my_deck.deal()
-print(f"Drawn card: {drawn_card}")
         
